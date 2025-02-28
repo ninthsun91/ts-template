f0ece7ed9c82427f31427a9a824b0838747edd9e
Sewook Kim <ninthsun91@meshed3d.com>
2025-02-28 20:16:32 +0900

    Install prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,13 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
+  eslintConfigPrettier,
   ...tseslint.configs.recommended,
 ];
