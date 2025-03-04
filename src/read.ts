c9479da362c820a7a424ab44ac2ad9ead4bab869
Sewook Kim <ninthsun91@meshed3d.com>
2025-03-04 16:38:57 +0900

    fix: Disable verbatimModuleSyntax, enable esModuelInterop

--- src/read.ts
+++ src/read.ts
@@ -1,5 +1,5 @@
 import fs from 'node:fs/promises';
 
 export async function read(path: string): Promise<string> {
-  return await fs.readFile(path, 'utf-8');
+  return fs.readFile(path, 'utf-8');
 }
